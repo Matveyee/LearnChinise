b1c9f8d76eee34cee3ccec824983cd09f6f0c34d
matvey <matvey.karlov@inbox.ru>
2023-05-26 12:59:17 +0300

    sets onload

--- index.js
+++ index.js
@@ -10,42 +10,45 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const client = new MongoClient('mongodb+srv://vercel-admin-user:verceladmin@cluster0.sfz0dsq.mongodb.net/?retryWrites=true&w=majority');
 
-
+//connecting database
 try{
     client.connect();
     console.log('База данных поключена');
 }catch (e) {
     console.log(e);
 }
+//connecting database
 
 const users = client.db().collection('users');
-let parsedData;
+const sets = client.db().collection('sets');
 
-
+//exclusions of adresses
 let adresses = {
     '/' : path.join(__dirname,'HTML','index.html'),
     '/profile_icon.png' : path.join(__dirname, 'images', 'profile_icon.png'),
+    '/main.css' : path.join(__dirname, 'CSS', 'main.css'),
 }
+//exclusions of adresses
 
 const server = http.createServer( (req,res) => {
 
-
+    //function that reads urls
     function readUrl( url, info = 0) {
         if ( ( url != '/favicon.ico' )) {
             if ( !Object.keys(adresses).includes(url)) {
 
-            fs.readFile(  path.join(__dirname, 'HTML', url.split('/')[1]+'.html'),'utf8', (err,data) => {
-                if (err) {throw err};
-                if ( info !== 0 ) {
+                fs.readFile(  path.join(__dirname, 'HTML', url.split('/')[1]+'.html'),'utf8', (err,data) => {
+                    if (err) {throw err};
+                    if ( info !== 0 ) {
 
-                        for(let el of Object.keys(placeHolders)) {
-                            data = data.replace(el,placeHolders[el])
+                            for(let el of Object.keys(info)) {
+                                data = data.replace(el,info[el])
 
-                        }
+                            }
 
-                }
-                res.writeHead(200, {"Content-type" : "text/html"});
-                res.end(data)
+                    }
+                    res.writeHead(200, {"Content-type" : "text/html"});
+                    res.end(data)
             })  
 
             } else {
@@ -53,27 +56,35 @@ const server = http.createServer( (req,res) => {
                     if (err) {throw err};
                     if ( info !== 0 ) {
 
-                        for(let el of Object.keys(placeHolders)) {
-                            data = data.replace(el,placeHolders[el])
+                        for(let el of Object.keys(info)) {
+                            data = data.replace(el,info[el])
 
                         }
 
-                }
-                    res.writeHead(200, {"Content-type" : "text/html"});
+                }   let basename;
+                    if (url == '/') {
+                        basename = 'html'
+                    }else {
+                        basename = url.split('.')[1];
+                    }
+                    
+                    res.writeHead(200, {"Content-type" : `text/${basename}`});
                     res.end(data)
                 })   
             }
             
         }
     }
+    //function that reads urls
 
-    console.log(req.url);
-    console.log(req.method)
-    
+    let place;
+    let placeHolders = {};
+    let parsedData;
+    //proessing request's methods
     if (req.method === 'POST') {
+        // parsing data
         let data = '';
-        var placeHolders;
-        
+        let user;
         
         req.on('data', chunk => {
         data += chunk.toString();
@@ -81,30 +92,76 @@ const server = http.createServer( (req,res) => {
         
         req.on('end', () => {
             parsedData = querystring.parse(data);
-             placeHolders = {
-                '{username}' : parsedData.username
-
-            }
+        // parsing data
+            console.log(parsedData)
+            //work with forms and db
             if (req.url == '/regout') {
                 users.insertOne(parsedData);
-                readUrl('/logout', placeHolders)
+
+                async function retUser() {
+                user =  await users.findOne( {username: parsedData.username, password: parsedData.password} );
+
+                return user;
+                }
+
+                retUser().then( result => {
+                    placeHolders['username'] = result.username;
+
+                readUrl('/logout', placeHolders);
+                })
+  
+            }else if (req.url == '/logout') {
+                //..
+            }else if ( req.url == '/newSet' ) {
+                console.log('here')
+                
+                async function makeArray() {
+                    await sets.insertOne( { Words : parsedData.words, Name: parsedData.setName });
+                    let arr = await sets.find().toArray();
+                    return arr;
+                    
+                }
+                makeArray().then( (arr) => {
+                    for ( let el of arr ) {
+                        place = place + `<div class="set" >
+                        <p align="center" >${el.Name}</p>
+                        <p align="center" >${el.Words}</p>
+                    </div>
+                    <br>`
+                    }
+                    placeHolders['{sets}'] = place
+                    readUrl('/sets', placeHolders);
+                } );
+                //work with forms and db
+                
             }
-            if (req.url == '/logout') {
-                readUrl('/logout', placeHolders)
-            }
-            console.log(parsedData);
             });
-        
-        
-        
-        
     }else if (req.method === 'GET') {
         if (req.url == '/regout') {
             readUrl('/logout')
+       }else if ( req.url == '/sets' ) {
+            async function makeArray2() {
+                let arr = await sets.find().toArray();
+                return arr;
+                    
+            }
+                makeArray2().then( (arr) => {
+                    for ( let el of arr ) {
+                        place = place + `<div class="set" >
+                        <p align="center" > <a href='/set_${el.Name}' >${el.Name}</a></p>
+                        <p align="center" >${el.Words}</p>
+                    </div>
+                    <br>`
+                    }
+                    placeHolders['{sets}'] = place
+                    readUrl('/sets', placeHolders);
+                } );
        }else {
-            readUrl(req.url)
+        readUrl(req.url)
        }
+
     }
+    //processing request's methods
 
 
 })
